e4e8a7455c58d97e7e64c4aac88b562b5693c4bc
iShadowBroker <ishadowbroker@proton.me>
2025-01-11 12:02:34 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-sudo pacman -S --needed --noconfirm git
+sudo pacman -S --needed --noconfirm base-devel git
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -si
 cd ..
 rm -rf yay-bin
 
-yay -Syu --noconfirm --needed git stow python python-questionary python-rich
+yay -Syu --noconfirm --needed git stow python python-questionary python-rich fish
 python ./dots.py
